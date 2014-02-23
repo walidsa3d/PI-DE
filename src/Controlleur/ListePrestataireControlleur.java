868a450ca2235b10a411df8ae68452e0a5c96497
Mohamed <mohamed.boujeh@gmail.com>
2014-02-23 17:21:04 +0100

    lister prestataire controlleur

---
+++ src/Controlleur/ListePrestataireControlleur.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlleur;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+import DAO.PrestataireDao;
+import Connection.MyConnection;
+import Entites.Prestataire;
+/**
+ *
+ * @author triki
+ */
+public class ListePrestataireControlleur extends AbstractTableModel  {
+   
+
+    String[] headers ={"IdPrestataire", "Nom","Prenom","Adresse","Mail","Mot De passe","Telephone"};
+    List<Prestataire> MesPrestataires = new ArrayList<Prestataire>();
+
+    public ListePrestataireControlleur() {
+        PrestataireDao dao = new PrestataireDao();
+        MesPrestataires = dao.GetAllPrestataire();
+    }
+
+    @Override
+    public int getRowCount() {
+        return MesPrestataires.size();
+
+    }
+
+    @Override
+    public int getColumnCount() {
+        return headers.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+
+
+        switch (columnIndex) {
+            case 0:
+                return MesPrestataires.get(rowIndex).getId_Prestataire();
+            case 1:
+                return MesPrestataires.get(rowIndex).getNom();
+            case 2:
+                return MesPrestataires.get(rowIndex).getPrenom();
+            case 3:
+                return MesPrestataires.get(rowIndex).getMail();
+            case 4:
+                return MesPrestataires.get(rowIndex).getAdress();
+            case 5:
+                return MesPrestataires.get(rowIndex).getMot_De_Passe();
+            case 6:
+                return MesPrestataires.get(rowIndex).getTel();
+
+
+            default:
+                return null;
+        }
+    }
+    
+    @Override
+    public String getColumnName(int column) {
+        return headers[column];
+    }
+    
+    
+}
