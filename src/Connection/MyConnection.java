fb1d2975c435e196d35a5461239e1bd46e67c403
Zimbala <mohamedamine.selmene@esprit.tn>
2014-03-05 13:49:47 +0100

    final -1

---
+++ src/Connection/MyConnection.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Sony
+ */
+public class MyConnection {
+    private static final String Driver ="com.mysql.jdbc.Driver";
+    private static  final String url ="jdbc:mysql://localhost:3306/wedding";
+    private  static final String user = "root";
+    private static  final String pwd ="";
+    
+    private static Connection con;
+      private MyConnection() {
+    }
+    public Connection etablir()
+    {
+        try {
+            Class.forName(Driver);
+            con = DriverManager.getConnection(url, user, pwd);
+            System.out.println("connexion etabli");
+            
+            
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Erreur de driver"+ex.getMessage());
+            
+        } catch (SQLException ex) {
+            System.out.println("erreur de connexion"+ex.getMessage());
+           
+        }
+        return con;
+    }
+    
+    public static Connection getInstance()
+    {
+        if (con==null)
+            con=new MyConnection().etablir();
+        return con;
+    }
+    
+    
+    
+}
